8299b548a1c6e1bc78d7be97202fc836969abc06
Asela <ecomsearchonline@gmail.com>
2021-09-29 11:23:10 +0530

    fixed

--- src/components/ActivityFeed/ActivityFeed.js
+++ src/components/ActivityFeed/ActivityFeed.js
@@ -114,39 +114,39 @@ const resolveTransitionMessage = (
   const isOwnTransition = transition.by === ownRole;
   const currentTransition = transition.transition;
   const displayName = otherUsersName;
-  console.log("this is the feed",currentTransition)
+
   switch (currentTransition) {
 
-    // case TRANSITION_CONFIRM_PAYMENT:
-    //   return isOwnTransition ? (
-    //     <FormattedMessage id="ActivityFeed.ownTransitionRequest" values={{ listingTitle }} />
-    //   ) : (
-    //     <FormattedMessage
-    //       id="ActivityFeed.transitionRequest"
-    //       values={{ displayName, listingTitle }}
-    //     />
-    //   );
-    // case TRANSITION_ACCEPT:
-    //   return isOwnTransition ? (
-    //     <FormattedMessage id="ActivityFeed.ownTransitionAccept" />
-    //   ) : (
-    //     <FormattedMessage id="ActivityFeed.transitionAccept" values={{ displayName }} />
-    //   );
-    // case TRANSITION_DECLINE:
-    //   return isOwnTransition ? (
-    //     <FormattedMessage id="ActivityFeed.ownTransitionDecline" />
-    //   ) : (
-    //     <FormattedMessage id="ActivityFeed.transitionDecline" values={{ displayName }} />
-    //   );
-    // case TRANSITION_EXPIRE:
-    //   return txRoleIsProvider(ownRole) ? (
-    //     <FormattedMessage id="ActivityFeed.ownTransitionExpire" />
-    //   ) : (
-    //     <FormattedMessage id="ActivityFeed.transitionExpire" values={{ displayName }} />
-    //   );
-    // case TRANSITION_CANCEL:
-    //   return <FormattedMessage id="ActivityFeed.transitionCancel" />;
     case TRANSITION_CONFIRM_PAYMENT:
+      return isOwnTransition ? (
+        <FormattedMessage id="ActivityFeed.ownTransitionRequest" values={{ listingTitle }} />
+      ) : (
+        <FormattedMessage
+          id="ActivityFeed.transitionRequest"
+          values={{ displayName, listingTitle }}
+        />
+      );
+    case TRANSITION_ACCEPT:
+      return isOwnTransition ? (
+        <FormattedMessage id="ActivityFeed.ownTransitionAccept" />
+      ) : (
+        <FormattedMessage id="ActivityFeed.transitionAccept" values={{ displayName }} />
+      );
+    case TRANSITION_DECLINE:
+      return isOwnTransition ? (
+        <FormattedMessage id="ActivityFeed.ownTransitionDecline" />
+      ) : (
+        <FormattedMessage id="ActivityFeed.transitionDecline" values={{ displayName }} />
+      );
+    case TRANSITION_EXPIRE:
+      return txRoleIsProvider(ownRole) ? (
+        <FormattedMessage id="ActivityFeed.ownTransitionExpire" />
+      ) : (
+        <FormattedMessage id="ActivityFeed.transitionExpire" values={{ displayName }} />
+      );
+    case TRANSITION_CANCEL:
+      return <FormattedMessage id="ActivityFeed.transitionCancel" />;
+    case TRANSITION_COMPLETE:
       // Show the leave a review link if the state is delivered and if the current user is the first to leave a review
       const reviewPeriodJustStarted = txIsDelivered(transaction);
 
